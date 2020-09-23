9ae3dcf6d062dfcc0d0ae803a8fd7e2fa8f4c158
Jaderson Barreto Sathler <jbsathler@gmail.com>
2020-09-23 00:49:34 -0300

    Commiting HTML

---
+++ src/components/ProgressBar.svelte
@@ -0,0 +1,18 @@
+<style>
+    .progress-container {
+        margin: 20px 0;
+        border: solid #C2C2C2 1px;
+    }
+    .progress-bar {
+        height: 30px;
+        background-color: #4DC737;
+    }
+</style>
+
+<div bp="grid">
+    <div bp="offset-5@md 4@md 12@sm" class="progress-container">
+        <div class="progress-bar" style="width:20%">
+            <span class="sr-only"></span>
+        </div>
+    </div>
+</div>
